92170feb17a85e460d558b99affb5b57756c3de9
SRKads1998 <kadonoffs@gmail.com>
2026-05-15 09:41:36 -0700

    final repo structure

--- SELECT.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    h."Track Name",
-    h."Artist Name(s)",
-    h."Duration (ms)"
-FROM hip_hop_track_list h
-JOIN song_list s ON h.song_tracking_id = s.SongID
-ORDER BY h."Duration (ms)" DESC
-LIMIT 10;
